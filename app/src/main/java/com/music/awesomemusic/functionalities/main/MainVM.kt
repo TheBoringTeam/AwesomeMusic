bcd3825cd848363d29ca1f47d7ec54966a7c4540
HolkerDev <nick.penkovskiy@gmail.com>
2020-05-01 23:34:34 +0200

    Deleted imports

--- app/src/main/java/com/music/awesomemusic/functionalities/main/MainVM.kt
+++ app/src/main/java/com/music/awesomemusic/functionalities/main/MainVM.kt
@@ -1,14 +1,10 @@
 package com.music.awesomemusic.functionalities.main
 
-import android.util.Log
 import androidx.databinding.ObservableField
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.music.awesomemusic.data.model.responses.MusicResponse
 import com.music.awesomemusic.data.repository.UserRepository
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import javax.inject.Inject
 
 class MainVM @Inject constructor(var service: UserRepository) : ViewModel() {
