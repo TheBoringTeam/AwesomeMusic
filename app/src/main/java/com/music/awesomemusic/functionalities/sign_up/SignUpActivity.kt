de49d92e763909c51627162469d47a3e27ef81c5
HolkerDev <nick.penkovskiy@gmail.com>
2020-05-10 22:32:18 +0200

    Refactor signUp view

--- app/src/main/java/com/music/awesomemusic/functionalities/sign_up/SignUpActivity.kt
+++ app/src/main/java/com/music/awesomemusic/functionalities/sign_up/SignUpActivity.kt
@@ -4,18 +4,19 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.music.awesomemusic.R
 import com.music.awesomemusic.databinding.ActivitySignUpBinding
-import com.music.awesomemusic.databinding.ActivityStartBinding
 import com.music.awesomemusic.di.Injectable
 import com.music.awesomemusic.di.ViewModelInjectionFactory
 import com.music.awesomemusic.functionalities.login.LoginActivity
-import com.music.awesomemusic.functionalities.start.StartActivity
-import com.music.awesomemusic.functionalities.start.StartVM
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 import javax.inject.Inject
 
 class SignUpActivity : AppCompatActivity(), Injectable {
@@ -38,7 +39,8 @@ class SignUpActivity : AppCompatActivity(), Injectable {
 
     private fun initBinding() {
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
-        _viewModel = ViewModelProviders.of(this, viewModelInjectionFactory).get(SignUpVM::class.java)
+        _viewModel =
+            ViewModelProviders.of(this, viewModelInjectionFactory).get(SignUpVM::class.java)
         _binding.viewModel = _viewModel
     }
 
@@ -54,19 +56,31 @@ class SignUpActivity : AppCompatActivity(), Injectable {
                 }
                 is SignUpState.ValidationError -> {
                     //TODO: Create some kinda of message on the screen for validation errors
-                    Toast.makeText(applicationContext, event.message, Toast.LENGTH_SHORT).show()
+                    showValidationError(event.message)
                 }
             }
         })
     }
 
     private fun showRegistrationSuccess() {
-        TODO("Implement alert that shows success registration message.")
+        // TODO("Implement alert that shows success registration message.")
+        goToLogin()
     }
 
     private fun goToLogin() {
         val loginIntent = Intent(this, LoginActivity::class.java)
         startActivity(loginIntent)
         finish()
+    }
+
+    private fun showValidationError(errorMessage: String) {
+        sign_up_activity_tv_validation_error.text = errorMessage
+        sign_up_activity_tv_validation_error.visibility = View.VISIBLE
+        sign_up_activity_tv_validation_error.startAnimation(
+            AnimationUtils.loadAnimation(
+                this,
+                R.anim.shake
+            )
+        )
     }
 }
